82ed9295cad3352d280b5b791e31c448ef32af56
Chadrick Mahaffey <chadrick.mahaffey@royaltechnologies.com>
2017-03-01 15:06:41 -0500

    Adding ValidationResults Component (not done yet)

---
+++ src/app/components/_common/validation-results/validation-results.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'validation-results',
+    templateUrl: './app/_common/validation-results/validation-results.component.html'
+})
+export class ValidationResults implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
